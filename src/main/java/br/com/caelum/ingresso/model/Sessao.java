515041f2840b55e450565d30ef25c0142504d3a4
Raul Coan <rvc.coan@gmail.com>
2019-05-18 16:51:14 -0300

    entidade Sessao mapeada

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,12 +2,28 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+
+@Entity
 public class Sessao {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	private LocalTime horario;
+	
+	@ManyToOne
 	private Sala sala;
+
+	@ManyToOne
 	private Filme filme;
 
+	private LocalTime horario;
+	
 	public Integer getId() {
 		return id;
 	}
@@ -38,6 +54,16 @@ public class Sessao {
 
 	public void setSala(Sala sala) {
 		this.sala = sala;
+	}
+
+	public Sessao(Sala sala, Filme filme, LocalTime horario) {
+		super();
+		this.sala = sala;
+		this.filme = filme;
+		this.horario = horario;
+	}
+	
+	public Sessao() {
 	}
 
 }
